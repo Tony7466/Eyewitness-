9f76995e1fd65a7c469366ad423d335b71485162
Sophie Brun <sophie@freexian.com>
2016-02-03 16:45:35 +0100

    Imported Upstream version 2.1.1

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 EyeWitness
 ======
 
-EyeWitness is designed to take screenshots of websites, provide some server header info, and identify default credentials if possible.
+EyeWitness is designed to take screenshots of websites, RDP services, and open VNC servers, provide some server header info, and identify default credentials if possible.
 
-Inspiration came from Tim Tomes's PeepingTom Script.  I just wanted to change some things, and then it became a thought exercise to write it myself.
+EyeWitness is designed to run on Kali Linux.  It will auto detect the file you give it with the -f flag as either being a text file with URLs on each new line, nmap xml output, or nessus xml output.  The -t (timeout) flag is completely optional, and lets you provide the max time to wait when trying to render and screenshot a web page.
 
-EyeWitness is designed to run on Kali Linux.  It will auto detect the file you give it with the -f flag as either being a text file with URLs on each new line, nmap xml output, or nessus xml output.  The -t (timeout) flag is completely optional, and lets you provice the max time to wait when trying to render and screenshot a web page.  The --open flag, which is optional, will open the URL in a new tab within iceweasel.
+A complete usage guide which documents EyeWitness features and its typical use cases is available here - https://www.christophertruncer.com/eyewitness-usage-guide/
+
+Supported Linux Distros:
+
+Kali Linux
+
+Debian 7+ (at least stable, looking into testing) (Thanks to @themightyshiv)
 
 
 E-Mail: EyeWitness [@] christophertruncer [dot] com
@@ -21,9 +27,11 @@ Usage:
 
 Examples:
 
-./EyeWitness -f urls.txt
+./EyeWitness -f urls.txt --web
 
-./EyeWitness -f urls.xml -t 8 --open
+./EyeWitness -f urls.xml -t 8 --headless
+
+./EyeWitness -f rdp.txt --rdp
 
 Thanks:
 Thanks to Jason Hill (@jasonhillva) for helping to get the xml parsing working.  And thanks to the group of guys I work with giving me their thoughts on new features I could add in, ways to optimize the code, and more.
--- setup/setup.sh
+++ setup/setup.sh
@@ -3,6 +3,7 @@
 # Global Variables
 userid=`id -u`
 osinfo=`cat /etc/issue|cut -d" " -f1|head -n1`
+eplpkg='http://linux.mirrors.es.net/fedora-epel/6/i386/epel-release-6-8.noarch.rpm'
 
 # Clear Terminal (For Prettyness)
 clear
@@ -21,30 +22,154 @@ if [ "${userid}" != '0' ]; then
   exit 1
 fi
 
+kali2test=`cat /etc/issue`
+
+if [ "${kali2test}" == "Kali GNU/Linux 2.0 \n \l" ]; then
+  osinfo="Kali2"
+fi
+
 # OS Specific Installation Statement
 case ${osinfo} in
+  # Kali 2 dependency Install
+  Kali2)
+    apt-get update
+    echo '[*] Installing Kali2 Dependencies'
+    apt-get install -y python-qt4 python-pip xvfb python-netaddr python-dev
+    echo '[*] Installing RDPY'
+    git clone https://github.com/ChrisTruncer/rdpy.git
+    cd rdpy
+    python setup.py install
+    cd ..
+    rm -rf rdpy
+    echo '[*] Installing Python Modules'
+    pip install fuzzywuzzy
+    pip install selenium
+    pip install python-Levenshtein
+    pip install pyasn1 --upgrade
+    pip install pyvirtualdisplay
+    cd ../bin/
+    MACHINE_TYPE=`uname -m`
+    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+      wget -O phantomjs https://www.christophertruncer.com/InstallMe/kali2phantomjs
+    else
+      wget -O phantomjs https://www.christophertruncer.com/InstallMe/phantom32kali2
+    fi
+    chmod +x phantomjs
+    cd ..
+  ;;
   # Kali Dependency Installation
   Kali)
+    apt-get update
     echo '[*] Installing Kali Dependencies'
-    apt-get install python-pip
-    echo
+    apt-get install -y python-qt4 python-pip xvfb python-netaddr python-dev
+    echo '[*] Installing RDPY'
+    git clone https://github.com/ChrisTruncer/rdpy.git
+    cd rdpy
+    python setup.py install
+    cd ..
+    rm -rf rdpy
     echo '[*] Installing Python Modules'
-    pip install Ghost.py python_qt_binding
-    echo
+    pip install fuzzywuzzy
+    pip install selenium
+    pip install python-Levenshtein
+    pip install pyasn1 --upgrade
+    pip install pyvirtualdisplay
+    cd ../bin/
+    MACHINE_TYPE=`uname -m`
+    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+      wget -O phantomjs https://www.christophertruncer.com/InstallMe/phantomjs
+    else
+      wget -O phantomjs https://www.christophertruncer.com/InstallMe/kali32phantomjs
+    fi
+    chmod +x phantomjs
+    cd ..
   ;;
-  # Debian Dependency Installation
+  # Debian 7+ Dependency Installation
   Debian)
+    apt-get update
     echo '[*] Installing Debian Dependencies'
-    apt-get install unzip cmake qt4-qmake python python-qt4 python-pip
+    apt-get install -y cmake qt4-qmake python xvfb python-qt4 python-pip python-netaddr python-dev
+    echo '[*] Installing RDPY'
+    git clone https://github.com/ChrisTruncer/rdpy.git
+    cd rdpy
+    python setup.py install
+    cd ..
+    rm -rf rdpy
     echo
     echo '[*] Installing Python Modules'
-    pip install Ghost.py python_qt_binding
+    pip install python_qt_binding
+    pip install fuzzywuzzy
+    pip install selenium
+    pip install python-Levenshtein
+    pip install pyasn1
+    pip install pyvirtualdisplay
     echo
+    cd ../bin/
+    wget -O phantomjs https://www.christophertruncer.com/InstallMe/phantom_deb
+    chmod +x phantomjs
+    cd ..
+  ;;
+  # Ubuntu (tested in 13.10) Dependency Installation
+  Ubuntu)
+    apt-get update
+    echo '[*] Installing Ubuntu Dependencies'
+    apt-get install -y cmake qt4-qmake python python-qt4 python-pip xvfb python-netaddr python-dev
+    echo '[*] Installing RDPY'
+    git clone https://github.com/ChrisTruncer/rdpy.git
+    cd rdpy
+    python setup.py install
+    cd ..
+    rm -rf rdpy
+    echo
+    echo '[*] Installing Python Modules'
+    pip install python_qt_binding
+    pip install fuzzywuzzy
+    pip install selenium
+    pip install python-Levenshtein
+    pip install pyasn1
+    pip install pyvirtualdisplay
+    echo
+    cd ../bin/
+    wget -O phantomjs https://www.christophertruncer.com/InstallMe/phantom_ubu
+    chmod +x phantomjs
+    cd ..
+  ;;
+  # CentOS 6.5+ Dependency Installation
+  CentOS)
+    echo '[Warning]: EyeWitness on CentOS Requires EPEL Repository!'
+    read -p '[?] Install and Enable EPEL Repository? (y/n): ' epel
+    if [ "${epel}" == 'y' ]; then
+      rpm -ivh ${eplpkg}
+    else
+      echo '[!] User Aborted EyeWitness Installation.'
+      exit 1
+    fi
+    echo '[*] Installing CentOS Dependencies'
+    yum install cmake python python-pip PyQt4 PyQt4-webkit \
+                python-argparse xvfb python-netaddr python-dev
+    echo
+    echo '[*] Installing RDPY'
+    git clone https://github.com/ChrisTruncer/rdpy.git
+    cd rdpy
+    python setup.py install
+    cd ..
+    rm -rf rdpy
+    echo '[*] Installing Python Modules'
+    pip install python_qt_binding
+    pip install fuzzywuzzy
+    pip install selenium
+    pip install python-Levenshtein
+    pip install pyasn1
+    pip install pyvirtualdisplay
+    echo
+    cd ../bin/
+    wget http://www.christophertruncer.com/InstallMe/phantomjs
+    chmod +x phantomjs
+    cd ..
   ;;
   # Notify Manual Installation Requirement And Exit
   *)
-    echo "[Error]: ${OS} is not supported by this setup script."
-    echo '[Error]: To use EyeWitness, manually install python, PyQt4, and Ghost.py.'
+    echo "[Error]: ${osinfo} is not supported by this setup script."
     echo
     exit 1
 esac
